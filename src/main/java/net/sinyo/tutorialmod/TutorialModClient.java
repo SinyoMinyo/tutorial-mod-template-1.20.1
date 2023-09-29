e361dade3cd5888c4e60cd9175eae5e18ddc2afa
JaveMave <125689944+JaveMave@users.noreply.github.com>
2023-09-29 20:05:03 +0200

    idk

---
+++ src/main/java/net/sinyo/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.sinyo.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    public void onInitializeClient() {
+
+    }
+}
